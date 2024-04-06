8461ab9a85a38941e616e990d996e6acc63c7503
Ajinkya Kamble <ajinkyakamble18764@gmail.com>
2024-04-06 18:47:34 +0530

    initial commit

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _Db;
+        public CategoryController(ApplicationDbContext Db)
+        {
+            _Db = Db;
+            
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _Db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+           if(ModelState.IsValid)
+            {
+                _Db.Categories.Add(obj);
+                _Db.SaveChanges();
+              
+
+            }
+            TempData["Success"] = "Category Created Successfully";
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            Category categoryfromDb = _Db.Categories.Find(id);
+            if(categoryfromDb == null)
+            {
+
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _Db.Categories.Update(obj);
+                _Db.SaveChanges();
+
+            }
+            TempData["Success"] = "Category Updated Successfully";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            Category categoryfromDb = _Db.Categories.Find(id);
+            if (categoryfromDb == null)
+            {
+
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category obj = _Db.Categories.Find(id); 
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _Db.Categories.Remove(obj);
+            _Db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+
+         
+
+          
+        }
+
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] // Data annotation for primary key [] when we use ef core
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        // Make it absolute for ef core
+        public  string Name { get; set; }
+        [DisplayName("Order Name")]
+        public int Displayorder { get; set; }
+    }
+}
